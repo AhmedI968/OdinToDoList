f6b69a9abcddd4386dc35edd84ef018e86fda982
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-10 17:07:27 -0600

    Added functions to handle updating today and this week projects

--- src/modules/ProjectList.js
+++ src/modules/ProjectList.js
@@ -1,8 +1,11 @@
-// Purpose of this file is to display the list of projects in the project list page
+import Project from './Project.js';
 
 export default class ProjectList {
-    constructor(projects = []) {
-        this.projects = projects;
+    constructor() {
+        this.projects = [];
+        this.projects.push(new Project('Inbox', 'This is the default project. It cannot be deleted.'));
+        this.projects.push(new Project('Today', 'Tasks due today.'));
+        this.projects.push(new Project('This Week', 'Tasks due this week.'));
     }
 
     // Getters
@@ -19,7 +22,37 @@ export default class ProjectList {
         this.projects.push(project);
     }
 
+    getProject(title) {
+        return this.projects.find(project => project.getTitle() === title);
+    }
+
     removeProject(project) {
         this.projects = this.projects.filter(p => p !== project);
+    }
+
+    // Create a function that will update today and this week projects
+
+    updateToday() {
+        const today = this.getProject('Today');
+        const todayTasks = [];
+        this.projects.forEach(project => {
+            if (project.getTitle() === 'Today' || project.getTitle() === 'This Week') return;
+            project.getTasksDueToday().forEach(task => {
+                todayTasks.push(task);
+            });
+        });
+        today.setTasks(todayTasks);
+    }
+
+    updateThisWeek() {
+        const thisWeek = this.getProject('This Week');
+        const thisWeekTasks = [];
+        this.projects.forEach(project => {
+            if (project.getTitle() === 'Today' || project.getTitle() === 'This Week') return;
+            project.getTasksDueThisWeek().forEach(task => {
+                thisWeekTasks.push(task);
+            });
+        });
+        thisWeek.setTasks(thisWeekTasks);
     }
 };
