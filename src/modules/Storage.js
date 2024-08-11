98b903e0ee76f7df1a158818998868227060192a
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-11 10:12:49 -0600

    Create a project to handle getting AllProjects

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -7,4 +7,14 @@ export default class Storage {
     static saveAllProjects(projects) {
         localStorage.setItem('ProjectList', JSON.stringify(projects));
     }
+
+    static getAllProjects(projects) {
+        let projectList = Object.assign(new ProjectList(), JSON.parse(localStorage.getItem('ProjectList')));
+        projectList.setProjects(projectList.getProjects().map(project => {
+            let newProject = Object.assign(new Project(), project);
+            newProject.setTasks(newProject.getTasks().map(task => Object.assign(new Task(), task)));
+            return newProject;
+        }));
+        return projectList;
+    }
 };
